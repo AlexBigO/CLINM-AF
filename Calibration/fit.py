18e766efdf8c4ead79bec1ad7c41ef91182133a1
AlexBigO <alexandrebigot57@gmail.com>
2025-12-18 16:43:56 +0100

    Add preliminary version of fit script to retrieve MPV of amplitude and/or charge in real data

--- Calibration/fit.py
+++ Calibration/fit.py
@@ -8,6 +8,7 @@ author: Alexandre BIGOT, alexandre.bigot@iphc.cnrs.fr
 
 import sys
 
+import numpy as np
 import ROOT as r
 
 try:
@@ -19,6 +20,13 @@ try:
     from argparse import ArgumentParser
 except ModuleNotFoundError:
     print("Module 'argparse' is not installed. Please install it to run this script.")
+
+try:
+    from numpy import mean as npmean
+    from numpy import std as npstd
+except ModuleNotFoundError:
+    print("Module 'uproot' is not installed. Please install it to run this script.")
+
 
 try:
     import pandas as pd
@@ -40,11 +48,63 @@ except ModuleNotFoundError:
 
 try:
     sys.path.append("../Utils/")
-    from utils import fill_th1, get_h_config
+    from utils import fill_th1, get_h_config, configure_canvas
 except ModuleNotFoundError:
     print(
         "Module 'utils' is not in the '../Utils/' directory. Add it to run this script."
     )
+
+try:
+    sys.path.append("../Utils/")
+    from fit_utils import lorentzian, plot_fit, LanGaus, langaufun
+except ModuleNotFoundError:
+    print(
+        "Module 'fit_utils' is not in the '../Utils/' directory. Add it to run this script."
+    )
+
+try:
+    sys.path.append("../Utils/")
+    from style_formatter import set_global_style, set_object_style
+except ModuleNotFoundError:
+    print(
+        "Module 'style_formatter' is not in the '../Utils/' directory. Add it to run this script."
+    )
+
+
+NAME_PARS = ["MPV_landau", "sigma_landau", "mu_gauss", "sigma_gauss", "norm"]
+LABELS_HFIT_RES: list[str] = [
+    "FitStatus",
+    "Chi2OverNdf",
+    "Xmin",
+    "Xmax",
+    "x_{MPV}",
+    "#sigma_{MPV}",
+    "#rho_{#sigma_{l}, #sigma_{g}}",
+] + NAME_PARS
+
+
+def propagate_unc(sigma1: float, sigma2: float, rho12: float) -> float:
+    """
+    Helper function to propagate uncertainties
+
+    Parameters
+    ------------------------------------------------
+    - sigma1: float
+        Uncertainty on variable 1
+
+    - sigma2: float
+        Uncertainty on variable 2
+
+    - rho12: float
+        Correlation factor of sigma1 and sigma2
+
+    Returns
+    ------------------------------------------------
+    - sigma: float
+        Propagated uncertainty
+    """
+
+    return np.sqrt(sigma1**2 + sigma2**2 + 2 * rho12 * sigma1 * sigma2)
 
 
 # pylint:disable=too-many-locals
@@ -57,6 +117,7 @@ def main(name_config_file: str) -> None:
     - name_config_file: str
         Name of the YAML config file
     """
+    padleftmargin = 0.12
 
     # import configuration
     config: dict = {}
@@ -67,9 +128,11 @@ def main(name_config_file: str) -> None:
     name_infile = config["input"]["file"]
     name_tree: str = config["input"]["tree"]["name"]
     name_branches: list[str] = config["input"]["tree"]["branches"]
-
+    init_pars_fit = config["fit"]["pars"]
+    ranges_fit = config["fit"]["range"]
     h_configs = []
     histogram_cfg = config["histogram_config"]
+    histogram_cfg["range"] = ranges_fit
 
     # safeties
     if isinstance(name_branches, list) and isinstance(histogram_cfg["name"], list):
@@ -92,65 +155,202 @@ def main(name_config_file: str) -> None:
         hists.append(fill_th1(df[name], h_config))
 
     # configure output with fit results
-    name_outfile: str = config["output"]["name"]
+    name_outfile: str = config["output"]["file"]
+    name_pdf_outfile: str = name_outfile.replace(".root", str()) + ".pdf"
     outfile: r.TFile = r.TFile(name_outfile, "recreate")
     for hist in hists:
         hist.Write()
 
-    # config of fit result storage
-    labels_hfit_res: list[str] = [
-        "FitStatus",
-        "Chi2",
-        "NDF",
-        "Constant",
-        "Mean",
-        "Sigma",
-        "Xmin",
-        "Xmax",
-    ]
-
     hfit_results = []
+    hfit_correlations = []
+
+    exp: str = config["output"]["plot"]["info"]["exp"]
+    campaign: str = config["output"]["plot"]["info"]["campaign"]
+    particle_beam: str = config["output"]["plot"]["info"]["beam"]["particle"]
+    energy_beam: str = config["output"]["plot"]["info"]["beam"]["energy"]
+    run_number: str = str(config["output"]["plot"]["info"]["run"])
 
     # add a fitting procedure
-    for i, (hist, range_fit) in enumerate(zip(hists, config["fit"]["range"])):
-        mean: float = hist.GetMean()
-        sigma: float = hist.GetStdDev()
+    for i, (
+        hist,
+        name_branch,
+        name_data,
+        nbin,
+        range_fit,
+        init_par0,
+        init_par1,
+        init_par2,
+        init_par3,
+        init_par4,
+        ymin_canvas,
+        ymax_canvas,
+    ) in enumerate(
+        zip(
+            hists,
+            name_branches,
+            histogram_cfg["name"],
+            histogram_cfg["nbin"],
+            ranges_fit,
+            init_pars_fit[NAME_PARS[0]],
+            init_pars_fit[NAME_PARS[1]],
+            init_pars_fit[NAME_PARS[2]],
+            init_pars_fit[NAME_PARS[3]],
+            init_pars_fit[NAME_PARS[4]],
+            config["output"]["plot"]["ymin"],
+            config["output"]["plot"]["ymax"],
+        )
+    ):
 
-        func: r.TF1 = r.TF1("func", "gaus", *range_fit, 3)
-        func.SetParameter(1, mean)
-        func.SetParameter(2, sigma)
+        idx_to_test = None
 
-        fit_res = hist.Fit(func, "RS")
+        if i != idx_to_test and idx_to_test is not None:
+            continue
 
-        hfit_res = r.TH1D(
-            f"hFitRes{hist.GetName()}",
-            "",
-            len(labels_hfit_res),
-            0,
-            len(labels_hfit_res) - 1,
+        if "Charge" in name_branch:
+            padrightmargin = 0.09
+        else:
+            padrightmargin = 0.035  # default value
+
+        set_global_style(
+            padleftmargin=padleftmargin,
+            padrightmargin=padrightmargin,
+            padbottommargin=0.12,
+            padtopmargin=0.05,
+            titlesize=0.045,
+            labelsize=0.04,
+            maxdigits=3,
         )
 
+        Logger(f"Start fit of {name_branch}\n", "INFO")
+
+        data = df[name_branch].to_numpy()
+        mean = npmean(data)
+        std = npstd(data)
+
+        use_my_langaus = False
+
+        x = r.RooRealVar(name_branch, f"x{i}", *range_fit)
+        # Set #bins to be used for FFT sampling to 10000
+        x.setBins(10000, "cache")
+
+        # x.setRange("fit_range", *range_fit)
+        x.setBins(nbin)
+
+        frame = x.frame()
+
+        dataset = r.RooDataSet.from_numpy({name_branch: data}, [x])
+
+        # Construct landau(t,ml,sl)
+        if init_par0[0] == "auto":
+            ml = r.RooRealVar(NAME_PARS[0], NAME_PARS[0], mean, *init_par0[1:])
+        else:
+            ml = r.RooRealVar(NAME_PARS[0], NAME_PARS[0], *init_par0)
+        if init_par1[0] == "auto":
+            sl = r.RooRealVar(NAME_PARS[1], NAME_PARS[1], std, *init_par1[1:])
+        else:
+            sl = r.RooRealVar(NAME_PARS[1], NAME_PARS[1], *init_par1)
+        landau = r.RooLandau("lx", "lx", x, ml, sl)
+
+        # Construct gauss(t,mg,sg)
+        mg = r.RooRealVar(NAME_PARS[2], NAME_PARS[2], *init_par2)
+        mg.setConstant(True)
+        sg = r.RooRealVar(NAME_PARS[3], NAME_PARS[3], *init_par3)
+        gauss = r.RooGaussian("gauss", "gauss", x, mg, sg)
+
+        # Construct landau (x) gauss
+        lxg = r.RooFFTConvPdf("lxg", "landau (X) gauss", x, landau, gauss)
+        # add norm
+        norm = r.RooRealVar(NAME_PARS[4], NAME_PARS[4], *init_par4)
+        model = r.RooAddPdf("model", "model", [lxg], [norm])
+
+        # TODO
+        if use_my_langaus:
+            # func_langaus = r.TF1("func_langaus", langaufun, xmin, xmax, 4)
+            model = r.RooFormulaVar(
+                "func_langaus", langaufun, r.RooArgList(x, ml, sl, sg, norm)
+            )
+
+        fit_res = model.fitTo(
+            dataset, r.RooFit.NumCPU(10), Save=True
+        )  # , Range="fit_range"        )
+
+        hfit_correlation = fit_res.correlationHist()
+        hfit_correlation.SetName(f"hFitCorrelation{name_data}")
+        hfit_correlations.append(hfit_correlation)
+
+        corr_sl_sg = hfit_correlation.GetBinContent(
+            hfit_correlation.GetXaxis().FindBin("sigma_landau"),
+            hfit_correlation.GetYaxis().FindBin("sigma_gauss"),
+        )
+
+        func_model = model.asTF(r.RooArgList(x))
+        # get most probable value (MPV)
+        x_mpv: float = func_model.GetMaximumX()
+        # compute uncertainty on MPV by doing some weighted sum in quadrature
+        s_mpv = propagate_unc(
+            fit_res.floatParsFinal().find("sigma_landau").getValV(),
+            fit_res.floatParsFinal().find("sigma_gauss").getValV(),
+            corr_sl_sg,
+        )
+
+        ##########
+        # Plot
+        xmin = range_fit[0]
+        xmax = range_fit[1]
+        ymin = 0
+        if ymin_canvas != "auto":
+            ymin = ymin_canvas
+
+        ymax = 1.05 * hist.GetMaximum()
+        if ymax_canvas != "auto":
+            ymax = ymax_canvas
+        title = f";{config['output']['plot']['label'][i]}; Entries;"
+        c = configure_canvas(f"c_{i}", xmin, ymin, xmax, ymax, title)
+        c.Update()
+        c.Draw()
+        # c = r.TCanvas(f"c_{i}", "My TCanvas", 600, 600)
+        dataset.plotOn(frame, MarkerStyle=r.kFullCircle)
+        model.plotOn(frame, LineColor=r.kAzure + 4, LineWidth=2, MoveToBack=True)
+        frame.Draw("same")
+        ##########
+
+        # Use fit result
+        hfit_res = r.TH1D(
+            f"hFitRes{name_data}",
+            "",
+            len(LABELS_HFIT_RES),
+            0,
+            len(LABELS_HFIT_RES) - 1,
+        )
         my_res = [
-            fit_res.Status(),
-            fit_res.Chi2(),
-            fit_res.Ndf(),
-            fit_res.Parameter(0),
-            fit_res.Parameter(1),
-            fit_res.Parameter(2),
+            fit_res.status(),
+            frame.chiSquare(),
             *range_fit,
+            x_mpv,
+            s_mpv,
+            corr_sl_sg,
         ]
+
         errors = [
             0,  # no error on fit status
             0,  # no error on chi2
-            0,  # no error on ndf
-            fit_res.ParError(0),
-            fit_res.ParError(1),
-            fit_res.ParError(2),
             0,  # no error on xmin
             0,  # no error on xmax
+            0,  # no error on x_mpv
+            0,  # no error on s_mpv
+            0,  # no error on corr_sl_sg
         ]
+
+        for ipar, name_par in enumerate(NAME_PARS):
+            if ipar == 2:
+                my_res.append(0)
+                errors.append(0)
+                continue
+            my_res.append(fit_res.floatParsFinal().find(name_par).getValV())
+            errors.append(fit_res.floatParsFinal().find(name_par).getAsymErrorHi())
+
         for ilabel, (label, res, error) in enumerate(
-            zip(labels_hfit_res, my_res, errors)
+            zip(LABELS_HFIT_RES, my_res, errors)
         ):
             ibin = ilabel + 1
             hfit_res.GetXaxis().SetBinLabel(ibin, label)
@@ -159,13 +359,106 @@ def main(name_config_file: str) -> None:
 
         hfit_results.append(hfit_res)
 
+        # add a legend
+        if "Charge" in name_branch:
+            leg = r.TLegend(padleftmargin + 0.45, 0.8, 0.87, 0.9)
+        else:
+            leg = r.TLegend(padleftmargin + 0.53, 0.8, 0.87, 0.9)
+        leg.SetTextSize(0.035)
+        leg.SetFillStyle(0)
+        leg.AddEntry(dataset, "Data", "p")
+        leg.AddEntry(model, "Landau * Gauss", "l")
+
+        leg.Draw()
+
+        # add information in TLatex
+        latex_clinm = r.TLatex()
+        xlatex_clinm = padleftmargin + 0.03  # 0.18
+        ylatex_clinm = 0.92
+        latex_clinm.SetNDC()
+        latex_clinm.SetTextSize(0.04)
+        latex_clinm.SetTextAlign(13)  # align at top
+        latex_clinm.SetTextFont(42)
+        latex_clinm.DrawLatex(xlatex_clinm, ylatex_clinm, exp)
+
+        latex_info = r.TLatex()
+        xlatex_info = padleftmargin + 0.03
+        ylatex_info_max = 0.92 - 0.06
+        latex_info.SetNDC()
+        latex_info.SetTextSize(0.03)
+        latex_info.SetTextAlign(13)  # align at top
+        latex_info.SetTextFont(42)
+        latex_info.DrawLatex(xlatex_info, ylatex_info_max, campaign)
+        latex_info.DrawLatex(
+            xlatex_info, ylatex_info_max - 0.05, f"{particle_beam} @ {energy_beam}"
+        )
+        latex_info.DrawLatex(xlatex_info, ylatex_info_max - 0.1, f"Run {run_number}")
+
+        latex_fitpars = r.TLatex()
+        xlatex_fitpars = (
+            padleftmargin + 0.55 if "Amplitude" in name_branch else padleftmargin + 0.5
+        )
+        ylatex_fitpars_max = 0.75
+        latex_fitpars.SetNDC()
+        latex_fitpars.SetTextSize(0.03)
+        latex_fitpars.SetTextAlign(13)  # align at top
+        latex_fitpars.SetTextFont(42)
+        latex_fitpars.DrawLatex(
+            xlatex_fitpars, ylatex_fitpars_max, f"#chi^{{2}} / ndf = {my_res[1]:.2f}"
+        )
+        # latex_fitpars.DrawLatex(
+        #     xlatex_fitpars,
+        #     ylatex_fitpars_max - 0.05,
+        #     f"m_{{l}} = {my_res[5]:.3f} #pm {errors[5]:.3f}",
+        # )
+        latex_fitpars.DrawLatex(
+            xlatex_fitpars,
+            ylatex_fitpars_max - 0.05,
+            f"#sigma_{{l}} = {my_res[8]:.3f} #pm {errors[8]:.3f}",
+        )
+        # latex_fitpars.DrawLatex(
+        #     xlatex_fitpars,
+        #     ylatex_fitpars_max - 0.15,
+        #     f"m_{{g}} = {my_res[7]:.3f} #pm {errors[7]:.3f}",
+        # )
+        latex_fitpars.DrawLatex(
+            xlatex_fitpars,
+            ylatex_fitpars_max - 0.10,
+            f"#sigma_{{g}} = {my_res[10]:.3f} #pm {errors[10]:.3f}",
+        )
+        latex_fitpars.DrawLatex(
+            xlatex_fitpars,
+            ylatex_fitpars_max - 0.15,
+            f"#rho_{{ #sigma_{{l}}, #sigma_{{ g }} }} = {my_res[6]:.3f}",
+        )
+        latex_fitpars.DrawLatex(
+            xlatex_fitpars,
+            ylatex_fitpars_max - 0.20,
+            f"MPV = {my_res[4]:.3f} #pm {my_res[5]:.3f}",
+        )
+
+        c.Update()
+        c.Draw()
+        c.Write()
+
+        if i == idx_to_test and idx_to_test is not None:
+            c.Print(name_pdf_outfile)
+            continue
+
+        if len(name_branches) == 1:
+            c.Print(name_pdf_outfile)
+        else:
+            if i == 0:
+                c.Print(f"{name_pdf_outfile}(")
+            elif i == len(name_branches) - 1:
+                c.Print(f"{name_pdf_outfile})")
+            else:
+                c.Print(name_pdf_outfile)
+
     # save in output file
-    for hist in hists:
-        name = hist.GetName()
-        hist.SetName(f"QA_{name}")
-        hist.Write()
-    for hfit_res in hfit_results:
+    for hfit_res, hfit_correlation in zip(hfit_results, hfit_correlations):
         hfit_res.Write()
+        hfit_correlation.Write()
     outfile.Close()
 
 
